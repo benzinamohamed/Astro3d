05dd378b10849bc967352a548d3944233415e169
benzinamohamed <am.benzina@ensta.edu.dz>
2025-03-11 14:30:43 +0100

    adding the first component

---
+++ src/components/ui/SolarSystem.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from 'react'
+import { Canvas } from '@react-three/fiber';
+import * as THREE from 'three';
+import { OrbitControls } from '@react-three/drei';
+function SolarSystem() {
+    return (
+    <div className="flex items-center justify-center ">
+
+      </div>
+    );
+}
+
+export default SolarSystem
